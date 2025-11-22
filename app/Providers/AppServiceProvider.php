200c10bf3ea6fe3d497fa8040a9e9752442d5803
moncef dante <dante2410@hotmail.fr>
2025-11-22 14:31:22 +0100

    add transfer funds to connected account commande + Schedule command run

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Services\CartService;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schedule::command('payout:vendors')
+        ->monthlyOn(1, '00:00')
+        ->withoutOverlapping();
+
+        Vite::prefetch(concurrency: 3);
     }
 }
